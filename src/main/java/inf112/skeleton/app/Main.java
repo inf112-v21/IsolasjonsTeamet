9b7fb3518a406e6dcb01b7c14a665845fa5dbefe
kasper.melheim <kasper.melheim@student.uib.no>
2021-02-06 23:00:35 +0100

    Added resourcefolder, and changes in HelloWorld and Main classes

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,6 +3,7 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+//Main class
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
