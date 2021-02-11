4fdc0fb28ac02a19d6d5d6c6502dc74731357cc4
nooralindeflaten <noora.lindeflaten@icloud.com>
2021-02-11 14:52:37 +0100

    pr

---
+++ src/main/java/inf112/isolasjonsteamet/roborally/actions/Direction.java
@@ -0,0 +1,8 @@
+package inf112.isolasjonsteamet.roborally.actions;
+
+public class Direction implements Action{
+    /**
+     * A Class that provides the actions with directions and positions of a player which is used to make the
+     * next move
+     */
+}
---
+++ src/main/java/inf112/isolasjonsteamet/roborally/actions/Reverse.java
@@ -0,0 +1,8 @@
+package inf112.isolasjonsteamet.roborally.actions;
+
+public class Reverse implements Action{
+
+    /**
+     * An Action that let's the robot go backwards
+     */
+}
