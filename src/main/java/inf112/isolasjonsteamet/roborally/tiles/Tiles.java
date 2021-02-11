0f45b7d308ef2e5ba2da02fda714acd7704f870b
espensvasand <esv018@uib.no>
2021-02-11 17:40:19 +0100

    Fix spacing and newline, added TODO for Tiles actions

--- src/main/java/inf112/isolasjonsteamet/roborally/tiles/Tiles.java
+++ src/main/java/inf112/isolasjonsteamet/roborally/tiles/Tiles.java
@@ -11,22 +11,13 @@ public class Tiles {
 
 	//public static final TileType FOO = new TileType(new MoveForward(1));
 
-    public static final TileType WALL =  new TileType();
-
-    public static final TileType HOLE =  new TileType();
-
-    public static final TileType LASER =  new TileType();
-
-    public static final TileType FLAG =  new TileType();
-
-    public static final TileType REPAIR_SITES_HAMMER_WRENCH =  new TileType();
-
-    public static final TileType REPAIR_SITES_WRENCH =  new TileType();
-
-    public static final TileType CONVEYOR_BELT = new TileType( new MoveForward(1));
-
+    public static final TileType HOLE =  new TileType(); //TODO: Add actions to this tile
+    public static final TileType LASER =  new TileType(); //TODO: Add actions to this tile
+    public static final TileType FLAG =  new TileType(); //TODO: Add actions to this tile
+    public static final TileType REPAIR_SITES_HAMMER_WRENCH =  new TileType(); //TODO: Add actions to this tile
+    public static final TileType REPAIR_SITES_WRENCH =  new TileType(); //TODO: Add actions to this tile
+    public static final TileType CONVEYOR_BELT = new TileType(new MoveForward(1));
     public static final TileType ROTATING_CONVEYOR_BELT_LEFT = new TileType(new RotateLeft());
-
     public static final TileType ROTATING_CONVEYOR_BELT_RIGHT = new TileType(new RotateRight());
 
 }
