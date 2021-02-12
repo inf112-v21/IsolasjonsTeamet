4a99b283bf60077e01471ea153e1ea43e65f3187
kasper.melheim <kasper.melheim@student.uib.no>
2021-02-12 13:23:12 +0100

    Solved checkstyle problemer and formatted the code for all files.

--- src/main/java/inf112/isolasjonsteamet/roborally/app/Main.java
+++ src/main/java/inf112/isolasjonsteamet/roborally/app/Main.java
@@ -8,12 +8,16 @@ import inf112.isolasjonsteamet.roborally.app.HelloWorld;
  * Main class to start a new application.
  */
 public class Main {
-    public static void main(String[] args) {
-        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("First Tile Map");
-        cfg.setWindowedMode(500, 500);
 
-        //Creates a new instance of HelloWorld, our game
-        new Lwjgl3Application(new HelloWorld(), cfg);
-    }
+	/**
+	 * Main method.
+	 */
+	public static void main(String[] args) {
+		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+		cfg.setTitle("First Tile Map");
+		cfg.setWindowedMode(500, 500);
+
+		//Creates a new instance of HelloWorld, our game
+		new Lwjgl3Application(new HelloWorld(), cfg);
+	}
 }
