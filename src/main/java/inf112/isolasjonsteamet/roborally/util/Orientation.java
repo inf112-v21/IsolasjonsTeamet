2d0b0b2080e9a39e6a137de33807848ef3167f58
Katrix <katrix97@hotmail.com>
2021-02-12 12:01:11 +0100

    Fix checkstyle error

--- src/main/java/inf112/isolasjonsteamet/roborally/util/Orientation.java
+++ src/main/java/inf112/isolasjonsteamet/roborally/util/Orientation.java
@@ -1,5 +1,8 @@
 package inf112.isolasjonsteamet.roborally.util;
 
+/**
+ * A orientation of something, indicating which way it's pointing.
+ */
 public enum Orientation {
 	NORTH, SOUTH, EAST, WEST
 }
