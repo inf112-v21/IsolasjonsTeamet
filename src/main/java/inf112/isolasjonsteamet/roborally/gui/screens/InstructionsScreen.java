dcfc8f5daeaa00cba1912e6847eb71db367ccd7f
Katrix <katrix97@hotmail.com>
2021-02-26 11:45:18 +0100

    Add proper stuff to instructions screen

--- src/main/java/inf112/isolasjonsteamet/roborally/gui/screens/InstructionsScreen.java
+++ src/main/java/inf112/isolasjonsteamet/roborally/gui/screens/InstructionsScreen.java
@@ -1,6 +1,10 @@
 package inf112.isolasjonsteamet.roborally.gui.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import inf112.isolasjonsteamet.roborally.gui.ScreenController;
@@ -21,14 +25,22 @@ public class InstructionsScreen extends StageScreen {
 		super.create();
 
 		var table = createRootTable();
-		table.add("How to play");
-		table.row().row();
+		table.add("How to play").colspan(2);
+		table.row();
 
-		table.add("Just play the game");
+		var instruction =
+				new Label("This is the board. Your goal is to get to the flag, "
+						+ "and avoid the hole. You move with WASD.", skin);
+		instruction.setWrap(true);
+		table.add(instruction).width(200);
+
+		var texture = new Texture(Gdx.files.internal("guis/instructions/board.png"));
+		var boardImage = new Image(texture);
+		table.add(boardImage).width(texture.getWidth() / 2F).height(texture.getHeight() / 2F).pad(15);
 		table.row();
 
 		var backButton = new TextButton("Back", skin);
-		table.add(backButton);
+		table.add(backButton).colspan(2);
 
 		backButton.addListener(new ChangeListener() {
 			@Override
