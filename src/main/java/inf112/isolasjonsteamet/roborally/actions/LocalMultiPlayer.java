c4bbe2a853b155c57f92570f5f2f56934d8750c5
nooralindeflaten <noora.lindeflaten@icloud.com>
2021-03-04 18:03:38 +0100

    different method to access player and keys to add new players

--- src/main/java/inf112/isolasjonsteamet/roborally/actions/LocalMultiPlayer.java
+++ src/main/java/inf112/isolasjonsteamet/roborally/actions/LocalMultiPlayer.java
@@ -1,71 +1,90 @@
 package inf112.isolasjonsteamet.roborally.actions;
 
+import inf112.isolasjonsteamet.roborally.players.Player;
+import inf112.isolasjonsteamet.roborally.players.PlayerImpl;
+import inf112.isolasjonsteamet.roborally.util.Coordinate;
+import inf112.isolasjonsteamet.roborally.util.Orientation;
+
 import java.awt.event.KeyEvent;
 import java.security.Key;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LocalMultiPlayer extends Player {
+public abstract class LocalMultiPlayer implements Player {
 
     public static int playerCount = 0;
-    public ArrayList<Player> players;
+    private String playerName;
+    private int life;
+    public ArrayList<Player> player;
+    private Orientation direction;
+    private Coordinate pos;
 
-    public static void newPlayer() {
-        return PlayerImpl.PlayerImpl();
+    /**
+     * Creates a new player with the information given in the playerImpl class
+     */
+
+ //   public boolean createPlayer(String name) {
+//        if (players.add(new Player(name))) {
+//            playerCount++;
+//            return true;
+//        }
+//        return false;
+//}
+
+    public void createPlayer(String playerName, Coordinate pos, Orientation orientation) {
+        this.playerName = playerName;
+        this.life = 5;
+        this.direction = orientation;
+        this.pos = pos;
     }
 
-    public boolean createPlayer(String name) {
-        if (players.add(new Player(name))) {
-            playerCount++;
-            return true;
-        }
-        return false;
+    public String getName() {
+
+        return playerName;
     }
 
     public void createPlayers(int quantity) {
         List<String> playerNames = new ArrayList<>(quantity);
     }
 
-    public boolean createPlayers(List<String> playerNames) {
+    public boolean createPlayers(ArrayList<Player> playerNames) {
         if (playerNames.isEmpty()) {
             throw new IllegalArgumentException("List of player names can't be empty");
         }
 
         boolean allAdded = true;
-        for (int i = 0; i < playerNames.size(); i++) {
-            if (!createPlayer(playerNames.get(i))) {
+        for (int i = 0; i < player.size(); i++) {
                 allAdded = false;
             }
-        }
-        return allAdded;
+        return allAdded = true;
     }
 
     public void keyPressed(KeyEvent e) {
         int c = e.getKeyCode();
 
         if (c == KeyEvent.VK_1){
-            createPlayer("Player 1");
+            createPlayer(playerName, pos, Orientation.NORTH);
             }
         if (c == KeyEvent.VK_2){
-            createPlayer( "Player 2");
+            createPlayer(playerName, pos, Orientation.NORTH);
             }
         if (c == KeyEvent.VK_3) {
-            createPlayer("Player 3");
+            createPlayer(playerName, pos, Orientation.NORTH);
             }
         if (c == KeyEvent.VK_4){
-            createPlayer("Player 4");
+            createPlayer(playerName, pos, Orientation.NORTH);
             }
         if (c == KeyEvent.VK_5){
-            createPlayer("Player 5");
+            createPlayer(playerName, pos, Orientation.NORTH);
         }
         if (c == KeyEvent.VK_6) {
-            createPlayer( "Player 6");
+            createPlayer(playerName, pos, Orientation.NORTH);
         }
         if (c == KeyEvent.VK_7) {
-            createPlayer("Player 7");
+            createPlayer(playerName, pos, Orientation.NORTH);
         }
         if (c == KeyEvent.VK_8) {
-            createPlayer( "Player 8");
+            createPlayer(playerName, pos, Orientation.NORTH);
         }
     }
 }
