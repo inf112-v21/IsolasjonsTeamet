5c9ee7497bcba0c67813167af996c1e358a2c51f
espensva <36599597+espensva@users.noreply.github.com>
2021-03-19 11:15:24 +0100

    Damage and repair robots (#28)
    * Renamed HelloWorld -> Game, and created interfaces for Player, Board and Action. First draft
    
    * Implemented method from interface Player in all classes that implements that interface.
    
    * Changed name of interfaces Board, Player and Action to BoardImpl, PlayerImpl and ActionImpl. Added abstract code for rotation in classes RotateLeft and RotateRight. Added abstract code for movement in Move, MoveForward and Reverse. Added Player class. Added Coords class that wil hold x and y values (position).
    
    * Changed name of interfaces BoardImpl, PlayerImpl and ActionImpl, to Board, Player and Action. Class Player changed to PlayerImpl.
    
    * Cleaning up and organizing code.
    
    * Formatting the code and adding comments, making sure CheckStyle passes.
    
    * Moved board code out from Game.java in a separate BoardImpl. java. Boards are now created by creating a new instance of BoardImpl.
    
    * Formatted code and added comments
    
    * Added ClientImpl that extends BoardImpl, ClientImpl will hold clientside code for creating boards. Changed keyUp to keyDown for faster movement. keyDown now calls to player.move() in PlayerImpl for moving the player (MoveForward(), Reverse() and rotation methods in Orientation will replace this shortly.
    
    * Added: When the user press R on the keyboard, the player will now change direction (rotate 90 degrees to the right). Moving happens with new MoveForward()
    
    * Seperate server and client concerns, implement movement
    
    Moved client rendering to ClientBoardImpl
    Added movement tests
    Moved around lot's of code
    
    * Implemented damage and repair of robots
    
    * Implemented damage and repair of robots
    
    * Added javadoc Destruction, KillRobot, RepairRobot
    
    * Tests for repair&damage robots
    
    * Tests for repair&damage robots
    
    * Checkstyle fix
    
    * Checkstyle fix
    
    * Checkstyle fix
    
    * Fixed test for killRobot
    
    * Fixed test for killRobot
    
    * Fixed test for killRobot
    
    * Fixed test for killRobot
    
    Co-authored-by: kasper.melheim <kasper.melheim@student.uib.no>
    Co-authored-by: Katrix <katrix97@hotmail.com>
    Co-authored-by: espensvasand <esv018@uib.no>

--- src/main/java/inf112/isolasjonsteamet/roborally/actions/RepairPlayer.java
+++ src/main/java/inf112/isolasjonsteamet/roborally/actions/RepairPlayer.java
@@ -4,12 +4,15 @@ import inf112.isolasjonsteamet.roborally.board.Board;
 import inf112.isolasjonsteamet.roborally.players.Player;
 
 /**
- * Repair player is an action that repairs a player x much according to cards they recieve.
+ * Repair player is an action that repairs a player x much according to cards they receive.
  */
 public class RepairPlayer implements Action {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void perform(Board board, Player player) {
-
+		player.repairRobot();
 	}
 }
