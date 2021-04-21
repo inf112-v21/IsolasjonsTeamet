bab9456cbfb3c5cf68d886d031a54b35b2e312dd
Katrix <katrix97@hotmail.com>
2021-04-21 14:50:25 +0200

    Fix laserSpec

--- src/test/java/inf112/isolasjonsteamet/roborally/LaserSpec.java
+++ src/test/java/inf112/isolasjonsteamet/roborally/LaserSpec.java
@@ -3,7 +3,7 @@ package inf112.isolasjonsteamet.roborally;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import inf112.isolasjonsteamet.roborally.board.BoardImpl;
-import inf112.isolasjonsteamet.roborally.players.PlayerImpl;
+import inf112.isolasjonsteamet.roborally.players.RobotImpl;
 import inf112.isolasjonsteamet.roborally.tiles.Tiles;
 import inf112.isolasjonsteamet.roborally.util.Coordinate;
 import inf112.isolasjonsteamet.roborally.util.Orientation;
@@ -15,73 +15,73 @@ public class LaserSpec {
 
     @Test
     public void laserDamagesPlayers() {
-        var player = new PlayerImpl(null, "foo", new Coordinate(0, 0), Orientation.NORTH);
+        var robot = new RobotImpl(null, new Coordinate(0, 0), Orientation.NORTH);
         var board = new BoardImpl(
-                ImmutableList.of(player),
                 ImmutableMap.of('l', ImmutableList.of(Tiles.GROUND, Tiles.LASER)),
                 "l"
         );
+        board.updateActiveRobots(ImmutableList.of(robot));
 
         board.checkValid();
         board.fireLaser();
 
-        assertEquals(1, player.getDamageTokens());
+        assertEquals(1, robot.getDamageTokens());
     }
 
     @Test
     public void laserEmitterDamagesPlayers() {
-        var player = new PlayerImpl(null, "foo", new Coordinate(0, 0), Orientation.NORTH);
+        var robot = new RobotImpl(null, new Coordinate(0, 0), Orientation.NORTH);
         var board = new BoardImpl(
-                ImmutableList.of(player),
                 ImmutableMap.of('l', ImmutableList.of(Tiles.GROUND, Tiles.LASER_EMITTER)),
                 "l"
         );
+		board.updateActiveRobots(ImmutableList.of(robot));
 
         board.checkValid();
         board.fireLaser();
 
-        assertEquals(1, player.getDamageTokens());
+        assertEquals(1, robot.getDamageTokens());
     }
 
     @Test
     public void laserDoesNotPassThroughPlayers() {
-        var player1 = new PlayerImpl(null, "foo", new Coordinate(0, 0), Orientation.NORTH);
-        var player2 = new PlayerImpl(null, "bar", new Coordinate(1, 0), Orientation.NORTH);
+        var robot1 = new RobotImpl(null, new Coordinate(0, 0), Orientation.NORTH);
+        var robot2 = new RobotImpl(null, new Coordinate(1, 0), Orientation.NORTH);
         var board = new BoardImpl(
-                ImmutableList.of(player1, player2),
                 ImmutableMap.of(
                         'e', ImmutableList.of(Tiles.GROUND, Tiles.LASER_EMITTER),
                         'l', ImmutableList.of(Tiles.GROUND, Tiles.LASER)
                 ),
                 "el"
         );
+		board.updateActiveRobots(ImmutableList.of(robot1, robot2));
 
         board.checkValid();
         board.fireLaser();
 
-        assertEquals(1, player1.getDamageTokens());
-        assertEquals(0, player2.getDamageTokens());
+        assertEquals(1, robot1.getDamageTokens());
+        assertEquals(0, robot2.getDamageTokens());
     }
 
     @Test
     public void laserCanComeFromMultipleDirections() {
-        var player1 = new PlayerImpl(null, "foo", new Coordinate(1, 0), Orientation.NORTH);
-        var player2 = new PlayerImpl(null, "bar", new Coordinate(2, 0), Orientation.NORTH);
-        var player3 = new PlayerImpl(null, "baz", new Coordinate(3, 0), Orientation.NORTH);
+        var robot1 = new RobotImpl(null, new Coordinate(1, 0), Orientation.NORTH);
+        var robot2 = new RobotImpl(null, new Coordinate(2, 0), Orientation.NORTH);
+        var robot3 = new RobotImpl(null, new Coordinate(3, 0), Orientation.NORTH);
         var board = new BoardImpl(
-                ImmutableList.of(player1, player2, player3),
                 ImmutableMap.of(
                         'e', ImmutableList.of(Tiles.GROUND, Tiles.LASER_EMITTER),
                         'l', ImmutableList.of(Tiles.GROUND, Tiles.LASER)
                 ),
                 "ellle"
         );
+		board.updateActiveRobots(ImmutableList.of(robot1, robot2, robot3));
 
         board.checkValid();
         board.fireLaser();
 
-        assertEquals(1, player1.getDamageTokens());
-        assertEquals(0, player2.getDamageTokens());
-        assertEquals(1, player3.getDamageTokens());
+        assertEquals(1, robot1.getDamageTokens());
+        assertEquals(0, robot2.getDamageTokens());
+        assertEquals(1, robot3.getDamageTokens());
     }
 }
